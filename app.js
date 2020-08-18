7664bca11a294d9fb6a6d79cc24e724130f27e40
AmarnathN <vayuteja@gmail.com>
2020-08-18 17:21:33 +0530

    Added mutation for posts

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
 const { ApolloServer } = require("apollo-server");
-
 const graphql = require("graphql-tag");
 const mongoose = require("mongoose");
 
+const Post = require("./models/Post");
+
 const typeDefs = graphql`
+  type Post {
+    id: ID!
+    body: String!
+    username: String!
+    createdAt: String!
+  }
+
   type Query {
     sayHi: String!
+    getPosts: [Post]
   }
   type Mutation {
     sayHiTo(name: String!): String!
+    createPost(body: String!): Post!
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => "Hello",
+    sayHi: async () => "Hello",
+    getPosts: async () => {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (e) {
+        throw new Error(e);
+      }
+    },
   },
   Mutation: {
+    //parent, args, context, info
     sayHiTo: (_, { name }) => {
       return `hi ${name}`;
     },
+    createPost: async (_, { body }, context, info) => {
+      const newPost = new Post({
+        body,
+        username: "Custom Username",
+        createdAt: new Date().toISOString(),
+      });
+      const post = await newPost.save();
+      return post;
+    },
   },
 };
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
