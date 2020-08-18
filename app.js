531c8479e750fec264c6acae07e6aa277e384045
AmarnathN <vayuteja@gmail.com>
2020-08-18 14:41:42 +0530

    Added data base connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require("apollo-server");
 
 const graphql = require("graphql-tag");
+const mongoose = require("mongoose");
 
 const typeDefs = graphql`
   type Query {
@@ -18,6 +19,15 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.listen({ port: 5001 }).then((result) => {
-  console.log(`server started at ${result.url}`);
-});
+mongoose
+  .connect("mongodb+srv://user_rw:Qwerty%4099@freemongocluster.dkqgp.mongodb.net/merng?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to DB");
+    return server.listen({ port: 5001 });
+  })
+  .then((result) => {
+    console.log(`server started at ${result.url}`);
+  });
